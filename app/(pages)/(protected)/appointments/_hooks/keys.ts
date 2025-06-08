a2116fec8198a4312ab8158b4ec7ca41432214f4
user <kielo_bash_mercado@dlsu.edu.ph>
2025-06-08 21:12:04 +0800

    feat: ui and fetching of queues, skip, call next, remove

--- app/(pages)/(protected)/appointments/_hooks/keys.ts
+++ app/(pages)/(protected)/appointments/_hooks/keys.ts
@@ -1,2 +1,3 @@
 export const KEY_GET_DOCTOR_TIMESLOTS = "get-doctor-timeslots";
 export const KEY_GET_DOCTOR_APPOINTMENTS = "get-doctor-appointments";
+export const KEY_GET_DOCTOR_QUEUES = "get-doctor-queues";
---
+++ app/api/queue/fetch-all/route.ts
@@ -0,0 +1,62 @@
+import { getDoctor } from "@/libs/user";
+import { NextResponse } from "next/server";
+import { prisma } from "@/prisma";
+import { Prisma } from "@prisma/client";
+
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url || "");
+
+    const doctorId = searchParams.get("doctorId") || "";
+    const statusFilter = searchParams.get("statusFilter") || "ALL";
+
+    if (!doctorId) {
+        return new NextResponse(JSON.stringify({ message: "No doctor specified" }), {
+            status: 400,
+        });
+    }
+
+    try {
+        const existingDoctor = await getDoctor({ doctorId });
+        if (!existingDoctor) {
+            return new NextResponse(JSON.stringify({ message: "No doctor found!" }), {
+                status: 400,
+            });
+        }
+
+        const filters: any = {
+            ...statusFilter && statusFilter !== "ALL" && {
+                status: statusFilter
+            },
+        }
+
+        const queues = await prisma.queue.findMany({
+            where: {
+                doctorId,
+                ...filters,
+            },
+            include: { appointment: true, patient: true, doctor: true },
+            orderBy: [
+                { date: "asc" }
+            ]
+        })
+
+        return new NextResponse(JSON.stringify({ message: "Fetched queues of doctor", payload: queues }), { status: 200 })
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            return new NextResponse(
+                JSON.stringify({ message: "Database error", error: error.message }),
+                { status: 500 }
+            );
+        }
+
+        return new NextResponse(
+            JSON.stringify({
+                message: "An unexpected error occurred",
+                error: (error as Error).message,
+            }),
+            { status: 500 }
+        );
+    } finally {
+        await prisma.$disconnect();
+    }
+}
